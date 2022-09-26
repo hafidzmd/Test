1a342e7e7f3b4039256e04e89f2ac3adcab25700
hafidzmd <114207516+hafidzmd@users.noreply.github.com>
2022-09-26 17:28:36 +0800

    Add files via upload

---
+++ Plotting Test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 21:32:29 2020
+
+@author: Hafidz MD
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt                          # This is a function for plotting a graph
+
+array = np.arange(20)
+
+#array2 = np.arange(array)
+
+array1 = array.shape
+
+print (array)
+print(array1)
+#plt.scatter (array,array1)
+
+'''
+x = ([1,2,3,4,5,6,7,8,9,10]) #The format is ([x],[y])
+y = ([1,1,2,2,3,3,4,4,5,5])
+
+plt.axis ([0,15,0,20])                                   # Args ([xmin,xmax,ymin,ymax])
+plt.ylabel('some numbers')
+
+plt.xlabel('Test')
+
+plt.scatter (x, y)
+'''
